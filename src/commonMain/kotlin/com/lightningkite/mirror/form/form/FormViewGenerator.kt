6daccabfcff4d9a8f7533db0a501a2c782f43b14
Joseph Ivie <josephivie@gmail.com>
2019-05-17 00:56:24 -0600

    Tests

--- src/commonMain/kotlin/com/lightningkite/mirror/form/form/FormViewGenerator.kt
+++ src/commonMain/kotlin/com/lightningkite/mirror/form/form/FormViewGenerator.kt
@@ -13,9 +13,12 @@ import com.lightningkite.mirror.form.FormState
 import com.lightningkite.mirror.form.GeneralRequest
 import com.lightningkite.mirror.form.PartFormViewGenerator
 import com.lightningkite.mirror.info.MirrorType
+import com.lightningkite.mirror.request.Request
 import com.lightningkite.reacktive.property.MutableObservableProperty
 import com.lightningkite.reacktive.property.ObservableProperty
 import com.lightningkite.reacktive.property.StandardObservableProperty
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class FormViewGenerator<T, DEPENDENCY : ViewFactory<VIEW>, VIEW>(
         val wraps: ViewGenerator<DEPENDENCY, VIEW>,
@@ -23,13 +26,15 @@ class FormViewGenerator<T, DEPENDENCY : ViewFactory<VIEW>, VIEW>(
         val onComplete: FormViewGenerator<T, DEPENDENCY, VIEW>.(T) -> Unit
 ) : ViewGenerator<DEPENDENCY, VIEW> by wraps {
 
+    val working = StandardObservableProperty(false)
+
     override fun generateActions(dependency: DEPENDENCY): VIEW? = with(dependency) {
         horizontal {
             val sub = wraps.generateActions(dependency)
             if (sub != null) {
                 -sub
             }
-            -imageButton(
+            -work(imageButton(
                     imageWithSizing = MaterialIcon.check.color(dependency.colorSet.foreground).withSizing(),
                     label = "Save",
                     importance = Importance.Low,
@@ -43,7 +48,7 @@ class FormViewGenerator<T, DEPENDENCY : ViewFactory<VIEW>, VIEW>(
                             //?
                         }
                     }
-            )
+            ), working)
         }
     }
 }
@@ -59,5 +64,30 @@ fun <T, DEPENDENCY : ViewFactory<VIEW>, VIEW> FormViewGenerator(
             wraps = PartFormViewGenerator(obs, type, generalRequest = generalRequest),
             obs = obs,
             onComplete = onComplete
+    )
+}
+
+fun <T : Request<OUT>, OUT, DEPENDENCY : ViewFactory<VIEW>, VIEW> DEPENDENCY.RequestViewGenerator(
+        type: MirrorType<T>,
+        startingWith: FormState<T> = FormState.empty(),
+        generalRequest: GeneralRequest,// = GeneralRequest(),
+        onComplete: FormViewGenerator<T, DEPENDENCY, VIEW>.(OUT) -> Unit
+): FormViewGenerator<T, DEPENDENCY, VIEW> {
+    return FormViewGenerator(
+            type = type,
+            startingWith = startingWith,
+            generalRequest = generalRequest,
+            onComplete = {
+                GlobalScope.launch {
+                    working.value = true
+                    try {
+                        val result = generalRequest.requestHandler!!.invoke(it)
+                        onComplete(result)
+                    } catch (e: Exception) {
+                        launchInfoDialog(title = "Error", message = e.message ?: "An unknown error occurred")
+                    }
+                    working.value = false
+                }
+            }
     )
 }
--- src/commonMain/kotlin/com/lightningkite/mirror/form/form/RequestViewGenerator.kt
+++ src/commonMain/kotlin/com/lightningkite/mirror/form/form/RequestViewGenerator.kt
@@ -1,45 +1,45 @@
-package com.lightningkite.mirror.form.form
-
-import com.lightningkite.koolui.builders.*
-import com.lightningkite.koolui.concepts.Importance
-import com.lightningkite.koolui.geometry.AlignPair
-import com.lightningkite.koolui.image.MaterialIcon
-import com.lightningkite.koolui.image.color
-import com.lightningkite.koolui.image.withSizing
-import com.lightningkite.koolui.views.ViewFactory
-import com.lightningkite.koolui.views.ViewGenerator
-import com.lightningkite.mirror.form.FormRequest
-import com.lightningkite.mirror.request.Request
-import com.lightningkite.reacktive.property.StandardObservableProperty
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-
-class RequestViewGenerator<T : Request<OUT>, OUT, DEPENDENCY : ViewFactory<VIEW>, VIEW>(
-        val formRequest: FormRequest<T>,
-        val wraps: ViewGenerator<DEPENDENCY, VIEW> = ReflectiveFormViewGenerator(formRequest),
-        val onResult: (OUT) -> Unit
-) : ViewGenerator<DEPENDENCY, VIEW> {
-
-    val isWorking = StandardObservableProperty(false)
-
-    override fun generate(dependency: DEPENDENCY): VIEW = wraps.generate(dependency)
-
-    override fun generateActions(dependency: DEPENDENCY): VIEW? = with(dependency) {
-        work(
-                view = imageButton(
-                        imageWithSizing = MaterialIcon.check.color(dependency.colorSet.foreground).withSizing(),
-                        label = "Run Request",
-                        importance = Importance.Low,
-                        onClick = {
-                            val request: T = formRequest.observable.value.valueOrNull ?: return@imageButton
-                            GlobalScope.launch {
-                                val result: OUT = (formRequest.general.requestHandler ?: return@launch).invoke(request)
-                                onResult(result)
-                            }
-                        }
-                ),
-                isWorking = isWorking
-        )
-
-    }
-}
+//package com.lightningkite.mirror.form.form
+//
+//import com.lightningkite.koolui.builders.*
+//import com.lightningkite.koolui.concepts.Importance
+//import com.lightningkite.koolui.geometry.AlignPair
+//import com.lightningkite.koolui.image.MaterialIcon
+//import com.lightningkite.koolui.image.color
+//import com.lightningkite.koolui.image.withSizing
+//import com.lightningkite.koolui.views.ViewFactory
+//import com.lightningkite.koolui.views.ViewGenerator
+//import com.lightningkite.mirror.form.FormRequest
+//import com.lightningkite.mirror.request.Request
+//import com.lightningkite.reacktive.property.StandardObservableProperty
+//import kotlinx.coroutines.GlobalScope
+//import kotlinx.coroutines.launch
+//
+//fun RequestViewGenerator<T : Request<OUT>, OUT, DEPENDENCY : ViewFactory<VIEW>, VIEW>(
+//        val formRequest: FormRequest<T>,
+//        val wraps: ViewGenerator<DEPENDENCY, VIEW> = ReflectiveFormViewGenerator(formRequest),
+//        val onResult: (OUT) -> Unit
+//) : ViewGenerator<DEPENDENCY, VIEW> {
+//
+//    val isWorking = StandardObservableProperty(false)
+//
+//    override fun generate(dependency: DEPENDENCY): VIEW = wraps.generate(dependency)
+//
+//    override fun generateActions(dependency: DEPENDENCY): VIEW? = with(dependency) {
+//        work(
+//                view = imageButton(
+//                        imageWithSizing = MaterialIcon.check.color(dependency.colorSet.foreground).withSizing(),
+//                        label = "Run Request",
+//                        importance = Importance.Low,
+//                        onClick = {
+//                            val request: T = formRequest.observable.value.valueOrNull ?: return@imageButton
+//                            GlobalScope.launch {
+//                                val result: OUT = (formRequest.general.requestHandler ?: return@launch).invoke(request)
+//                                onResult(result)
+//                            }
+//                        }
+//                ),
+//                isWorking = isWorking
+//        )
+//
+//    }
+//}
